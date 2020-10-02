85bdba231741ec767914b66e98abcc0aebc2425a
shubh666767 <ghatakshubhojit1@gmail.com>
2020-10-02 13:00:16 +0530

    to remove errors

---
+++ src/app/models/claimPageDisplay.ts
@@ -0,0 +1,10 @@
+export class ClaimPageDisplay{
+    status:boolean;
+    policyNo:number;
+    policyId:number;
+    policyType:string;
+    vehicleType:string;
+    manufacturer:string;
+    model:string;
+    registrationNumber:number;
+}
