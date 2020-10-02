f6e6b4a6f364e6165ca9af06ed721e5b8f4755a5
rishabh208gupta <rishabhgupta.52pp@gmail.com>
2020-10-02 17:40:55 +0530

    policy getting registerd

--- src/app/new-policy-details/new-policy-details.component.ts
+++ src/app/new-policy-details/new-policy-details.component.ts
@@ -14,7 +14,7 @@ export class NewPolicyDetailsComponent implements OnInit {
  
   newPolicy:NewPolicy = new NewPolicy();
   policyRegistrationStatus :PolicyRegistrationStatus= new PolicyRegistrationStatus();
-  constructor(private route:Router, private buyPolicyService:BuyPolicyService) { }
+  constructor(private router:Router, private buyPolicyService:BuyPolicyService) { }
 
   ngOnInit(): void {
     this.newPolicy.vehicle.vehicleId=parseInt(sessionStorage.getItem('vehicleId'));
@@ -27,6 +27,7 @@ export class NewPolicyDetailsComponent implements OnInit {
        this.policyRegistrationStatus=data;
        sessionStorage.setItem('policyNo',String(this.policyRegistrationStatus.policyNo))
        console.log(this.policyRegistrationStatus.policyNo);
+       this.router.navigate(['/make-payment']);
      })
   }
 
