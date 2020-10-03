a0c0f526ee489deda4e303b7c72beacee0212478
shivani <shivanijsh97@gmail.com>
2020-10-03 13:43:41 +0530

    added final formula for premium and 1 more field in html

--- src/app/calculate-premium/calculate-premium.component.ts
+++ src/app/calculate-premium/calculate-premium.component.ts
@@ -9,21 +9,27 @@ export class CalculatePremiumComponent implements OnInit {
   
   vehiclePrice:number;
   purchaseDate:Date;
-  policyAmount:number;
+  premiumRate:number;
   estimatedValue:number;
   age:number;
   timeDiff:any;
+  depreciatonRate:any;
+  idv:number;
+  planYear:number;
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
   calculatePremium(){
+    this.depreciatonRate=10;
     if(this.purchaseDate){
       const pdate = new Date(this.purchaseDate);
       this.timeDiff = Math.abs(Date.now()-pdate.getTime());
       //Used Math.floor instead of Math.ceil
       //so 26 years and 140 days would be considered as 26, not 27.
       this.age = Math.floor((this.timeDiff / (1000 * 3600 * 24))/365);
-    this.estimatedValue=+this.vehiclePrice+ +this.policyAmount+ +this.age;
+
+    this.idv=+this.vehiclePrice- +(this.vehiclePrice*this.age*this.depreciatonRate)/100;
+    this.estimatedValue=+(this.idv*this.premiumRate*this.planYear)/100;
   }
 }
 }
