f3bcb5c13d1918799897d60b9860acc048b0e45c
shubh666767 <ghatakshubhojit1@gmail.com>
2020-10-04 11:25:12 +0530

    added bill information in renew policy

--- src/app/renew-payment/renew-payment.component.ts
+++ src/app/renew-payment/renew-payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Bill } from '../models/bill-details';
 import { RenewPayment } from '../models/renewPolicy';
 import {RenewPolicyService} from '../services/renew-policy.service';
+import { Payment,PaymentStatus } from '../models/payment';
 @Component({
   selector: 'app-renew-payment',
   templateUrl: './renew-payment.component.html',
@@ -8,18 +10,23 @@ import {RenewPolicyService} from '../services/renew-policy.service';
 })
 export class RenewPaymentComponent implements OnInit {
   renewPayment:RenewPayment=new RenewPayment();
+  bill:Bill;
+  payment:Payment=new Payment();
   constructor(private renewPolicyService:RenewPolicyService) { }
 
   ngOnInit(): void {
+    this.bill=JSON.parse(sessionStorage.getItem('billing'));
+    this.payment.newPolicy.policyNo=parseInt(sessionStorage.getItem('policyNo'));
+    this.payment.amount=this.bill.amount;
   }
   renewPolicyPayment(){
-    //this.renewPayment.newPolicy.policyNo=Number(sessionStorage.getItem('policyNo'));
-    //this.renewPayment.policyDuration=Number(sessionStorage.getItem('policyDuration'));
+    this.renewPayment.newPolicy.policyNo=Number(sessionStorage.getItem('policyNo'));
+    this.renewPayment.policyDuration=Number(sessionStorage.getItem('policyDuration'));
     this.renewPayment.statusConfirmation=true;
-    this.renewPayment.amount=1000;
-    this.renewPayment.paymentMode="card";
-    this.renewPayment.newPolicy.policyNo=4;
-    this.renewPayment.policyDuration=3;
+    //this.renewPayment.amount=1000;
+    //this.renewPayment.paymentMode="card";
+    //this.renewPayment.newPolicy.policyNo=4;
+    //this.renewPayment.policyDuration=3;
 
     this.renewPolicyService.makePayment(this.renewPayment).subscribe(data=>{
       alert(JSON.stringify(data));
--- src/app/renew-policy/renew-policy.component.ts
+++ src/app/renew-policy/renew-policy.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import{RenewPolicy}from '../models/renewPolicy';
 import { Router } from '@angular/router';
 import{RenewPolicyService} from '../services/renew-policy.service';
+import { BuyPolicyService } from '../services/buy-policy.service';
+import { Bill } from '../models/bill-details';
 
 @Component({
   selector: 'app-renew-policy',
@@ -14,7 +16,8 @@ export class RenewPolicyComponent implements OnInit {
   policyDuration:number;
   status:boolean;
   statusMessage:string;
-  constructor( private route: Router,private renewPolicyService:RenewPolicyService) {}
+  bill:Bill;
+  constructor( private route: Router,private renewPolicyService:RenewPolicyService,private buyPolicyService:BuyPolicyService) {}
 
   ngOnInit(): void {
   //this.onClickRenewPolicy();
@@ -27,6 +30,10 @@ export class RenewPolicyComponent implements OnInit {
      this.status=data.status;
      this.statusMessage=data.statusMessage;
      if(data.status==true){
+       this.buyPolicyService.getBillDetails(this.policyNo).subscribe(response=>{
+        this.bill=response;
+        sessionStorage.setItem('billing', JSON.stringify(this.bill));
+       })
       this.route.navigate(['/renew-payment']);
      }
 
