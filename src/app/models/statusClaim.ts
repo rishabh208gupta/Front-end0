2f0e1561f965de567823080368cbb76b3c31adb1
shubh666767 <ghatakshubhojit1@gmail.com>
2020-10-04 10:44:43 +0530

    changed in model.ts

--- src/app/models/statusClaim.ts
+++ src/app/models/statusClaim.ts
@@ -4,14 +4,14 @@ export class StatusClaim{
     statusMessage:string;
     status:boolean;
 }
-<<<<<<< HEAD
+
 
 export class UserClaim{
     claimId:number;
     claimed:boolean;
     claimAmount:number;
-=======
+}
 export class RenewStatus extends Status{
     policyNo:number;
->>>>>>> d269370cd0b2a3939417315be4c3ca900c0bc602
+
 }
