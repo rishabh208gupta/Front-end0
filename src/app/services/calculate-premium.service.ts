9ba8137e5ae1fb208d5feccd4db728889fe0e331
shivani <shivanijsh97@gmail.com>
2020-10-05 14:56:37 +0530

    addded if else for age in calculate premium

--- src/app/services/calculate-premium.service.ts
+++ src/app/services/calculate-premium.service.ts
@@ -28,18 +28,27 @@ export class CalculatePremiumService {
       //Used Math.floor instead of Math.ceil
       //so 26 years and 140 days would be considered as 26, not 27.
       this.age = Math.floor((this.timeDiff / (1000 * 3600 * 24))/365);
-    if(vehiclePrice==50000 && (this.age>8 || this.depreciatedValue<0)){
+    if(vehiclePrice==50000 && (this.age>8 || this.depreciatedValue>vehiclePrice)){
       this.idv=10000;
-      this.estimatedValue=400;
+      if(planYear==1)
+        this.estimatedValue=400;
+      else if(planYear==2)
+        this.estimatedValue=800;
+      else if(planYear==3)
+        this.estimatedValue=1200;
     }
-    else if(vehiclePrice==400000 && (this.age>8 || this.depreciatedValue<0)){
+    else if(vehiclePrice==400000 && (this.age>8 || this.depreciatedValue>vehiclePrice)){
       this.idv=25000;
-      this.estimatedValue=2000;
+    if(planYear==1)
+      this.estimatedValue=2500;
+    else if(planYear==2)
+      this.estimatedValue=5000;
+    else if(planYear==3)
+      this.estimatedValue=7500;
     }
     else{
       this.idv=+vehiclePrice- +this.depreciatedValue;
       this.estimatedValue=+(this.idv*premiumRate*planYear)/100;
-
     }
     return [this.idv,this.estimatedValue];
     
