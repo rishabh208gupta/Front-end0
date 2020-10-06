ae20ba355c88218cb4b5b43d84bb931860f9c8c6
rishabh208gupta <rishabhgupta.52pp@gmail.com>
2020-10-06 12:10:56 +0530

    removed form builder

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @Component({
@@ -9,31 +8,21 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
-  forgotPasswordForm: FormGroup;
+  
   constructor(
-    private fb: FormBuilder,
+    
   ) { 
     
   }
 
   ngOnInit(): void {
-    this.buildForgotPasswordForm();
+   
   }
-  buildForgotPasswordForm(): void {
-    this.forgotPasswordForm = this.fb.group({
-      emailId:['',Validators.required],
-      captcha:['',Validators.required]
-    })
-  }
+  
 
 
   onSubmitClick(): void {
-    if (this.forgotPasswordForm.valid) {
-      alert('Password changed successfully')
-    } 
-    else {
-      alert('Please enter the correct fields')
-    }
+    
   }
 
 }
