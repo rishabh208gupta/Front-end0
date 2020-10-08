004199fb58940f3b8f25bedcee474f19d5a9d063
shubh666767 <ghatakshubhojit1@gmail.com>
2020-10-08 13:57:19 +0530

    added claim in fetch vehicle policy

--- src/app/fetch-vehicle-policy-details/fetch-vehicle-policy-details.component.ts
+++ src/app/fetch-vehicle-policy-details/fetch-vehicle-policy-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{PolicyVehicleDetailsService} from '../services/policy-vehicle-details.service';
+import{ClaimService} from '../services/claim.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-fetch-vehicle-policy-details',
   templateUrl: './fetch-vehicle-policy-details.component.html',
@@ -8,17 +10,29 @@ import{PolicyVehicleDetailsService} from '../services/policy-vehicle-details.ser
 export class FetchVehiclePolicyDetailsComponent implements OnInit {
 userPolicyNo:number;
 vehiclePolicyDetails:any;
-  constructor(private vehiclePolicy:PolicyVehicleDetailsService) { }
+reason:string;
+  constructor(private vehiclePolicy:PolicyVehicleDetailsService,private claimService:ClaimService
+                ,private router:Router) { }
 
   ngOnInit(): void {
-    alert(sessionStorage.getItem('userPolicyNo'));
+    //alert(sessionStorage.getItem('userPolicyNo'));
     this.userPolicyNo=parseInt(sessionStorage.getItem('userPolicyNo'));
     this.vehiclePolicy.fetchVehiclePolicyDetails(this.userPolicyNo).subscribe(data=>{
-      alert(JSON.stringify(data));
+     // alert(JSON.stringify(data));
       this.vehiclePolicyDetails=data;
 
     })
 
   }
+  onClaimClick(){
 
+   this.claimService.onClickingClaim(this.userPolicyNo,this.reason).subscribe(data=>{
+      sessionStorage.setItem('claimId',String(data.claimId));
+      sessionStorage.setItem('statusMessage',data.statusMessage);
+      sessionStorage.setItem('status',String(data.status));
+      // console.log(this.statusClaim[0]);
+      this.router.navigate(['/claim-successful']);
+   })
+
+}
 }
