35adb731e7a853e67266c8080476c91fad88c526
shubh666767 <ghatakshubhojit1@gmail.com>
2020-10-08 12:58:21 +0530

    added a new component fetch user policy

--- src/app/fetch-vehicle-policy-details/fetch-vehicle-policy-details.component.ts
+++ src/app/fetch-vehicle-policy-details/fetch-vehicle-policy-details.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import{PolicyVehicleDetailsService} from '../services/policy-vehicle-details.service';
 @Component({
   selector: 'app-fetch-vehicle-policy-details',
   templateUrl: './fetch-vehicle-policy-details.component.html',
   styleUrls: ['./fetch-vehicle-policy-details.component.css']
 })
 export class FetchVehiclePolicyDetailsComponent implements OnInit {
-
-  constructor() { }
+userPolicyNo:any;
+vehiclePolicyDetails:any;
+  constructor(private vehiclePolicy:PolicyVehicleDetailsService) { }
 
   ngOnInit(): void {
+    this.userPolicyNo=sessionStorage.getItem('userPolicyNo');
+    this.vehiclePolicy.fetchVehiclePolicyDetails(this.userPolicyNo).subscribe(data=>{
+      //alert(JSON.stringify(data));
+      this.vehiclePolicyDetails=data;
+
+    })
+
   }
 
 }
